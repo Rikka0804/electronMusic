f68054d9af82a47b3ecf93c3edbd0e18a3f3ce8e
Rikka0804 <xzh.0804@qq.com>
2025-09-03 10:39:20 +0800

    初始化项目

---
+++ src/main/frame/MainFrame.ts
@@ -0,0 +1,37 @@
+import { shell, BrowserWindow } from 'electron'
+import { join } from 'path'
+import { is } from '@electron-toolkit/utils'
+import icon from '../../../resources/icon.png?asset'
+export default class MainFrame {
+  #frame:any = null;
+  #width = 900;
+  #height = 670;
+  create() {
+    this.#frame = new BrowserWindow({
+      width: this.#width,
+      height: this.#height,
+      show: false,
+      autoHideMenuBar: true,
+      ...(process.platform === 'linux' ? { icon } : {}),
+      webPreferences: {
+        preload: join(__dirname, '../preload/index.js'),
+        sandbox: false
+      }
+    })
+
+    this.#frame.on('ready-to-show', () => {
+      this.#frame.show()
+    })
+
+    this.#frame.webContents.setWindowOpenHandler((details) => {
+      shell.openExternal(details.url)
+      return { action: 'deny' }
+    })
+
+    if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
+      this.#frame.loadURL(process.env['ELECTRON_RENDERER_URL'])
+    } else {
+      this.#frame.loadFile(join(__dirname, '../renderer/index.html'))
+    }
+  }
+}
